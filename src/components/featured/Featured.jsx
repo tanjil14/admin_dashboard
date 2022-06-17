00e60fe10e62219eef98600ff21ad4d347f60752
tanjil114 <tanjil14@cse.pstu.ac.bd>
2022-06-17 10:58:14 +0600

    progress bar adding

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,4 +1,5 @@
-import { MoreVert } from "@mui/icons-material";
+import { KeyboardArrowDown, MoreVert } from "@mui/icons-material";
+import { CircularProgressbar } from "react-circular-progressbar";
 import "./featured.scss";
 
 const Featured = () => {
@@ -10,7 +11,35 @@ const Featured = () => {
 
             </div>
             <div className="bottom">
-
+             <div className="featuredChart">
+                 <CircularProgressbar value={70} text={"70%"} strokeWidth={5}/>
+             </div>
+             <p className="title">Total salse made today</p>
+             <p className="amount">#420</p>
+             <p className="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+             <div className="summary">
+                 <div className="item">
+                     <div className="itemTitle">Target</div>
+                     <div className="itemResult negative">
+                         <KeyboardArrowDown fontSize="small"/>
+                         <div className="resultAmount">$12.4k</div>
+                     </div>
+                 </div>
+                 <div className="item">
+                     <div className="itemTitle">Last Week</div>
+                     <div className="itemResult positive">
+                         <KeyboardArrowDown fontSize="small"/>
+                         <div className="resultAmount">$12.4k</div>
+                     </div>
+                 </div>
+                 <div className="item">
+                     <div className="itemTitle">Last Month</div>
+                     <div className="itemResult positive">
+                         <KeyboardArrowDown fontSize="small"/>
+                         <div className="resultAmount">$12.4k</div>
+                     </div>
+                 </div>
+             </div>
             </div>
         </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "react-circular-progressbar/dist/styles.css";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
@@ -8,4 +9,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
