798aadfa7d0a13967610db6e1945901e3ab3ae9e
tanjil114 <tanjil14@cse.pstu.ac.bd>
2022-06-17 10:27:02 +0600

    adding charts

---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,19 @@
+import { MoreVert } from "@mui/icons-material";
+import "./featured.scss";
+
+const Featured = () => {
+    return (
+        <div className="featured">
+            <div className="top">
+                <h1 className="title">Total Revenue</h1>
+                <MoreVert fontSize="small"/>
+
+            </div>
+            <div className="bottom">
+
+            </div>
+        </div>
+    );
+};
+
+export default Featured;
