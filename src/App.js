a3fcbe41c2eebaf0876ecf8945f8521896e6c025
tanjil114 <tanjil14@cse.pstu.ac.bd>
2022-06-19 09:49:00 +0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { useContext } from "react";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import { AuthContext } from "./context/AuthContext.js";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { DarkModeContext } from "./context/darkModeContext.js";
 import { productInputs, userInputs } from "./formData.js";
 import Home from "./pages/home/Home.jsx";
@@ -11,76 +10,28 @@ import Single from "./pages/single/Single.jsx";
 import "./style/dark.scss";
 function App() {
   const { darkMode } = useContext(DarkModeContext);
-  const ProtectedRoute = ({ children }) => {
-    const { user } = useContext(AuthContext);
-    if (!user) {
-      return <Navigate to="/login" />;
-    }
-    return children;
-  };
   return (
     <div className={darkMode ? "app dark" : "app"}>
       <BrowserRouter>
         <Routes>
           <Route path="/">
+            <Route index element={<Home />} />
             <Route path="login" element={<Login />} />
-            <Route
-              index
-              element={
-                <ProtectedRoute>
-                  <Home />
-                </ProtectedRoute>
-              }
-            />
             <Route path="users">
-              <Route
-                index
-                element={
-                  <ProtectedRoute>
-                    <List />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path=":userId"
-                element={
-                  <ProtectedRoute>
-                    <Single />
-                  </ProtectedRoute>
-                }
-              />
+              <Route index element={<List />} />
+              <Route path=":userId" element={<Single />} />
               <Route
                 path="new"
-                element={
-                  <ProtectedRoute>
-                    <New inputData={userInputs} title="Add New User" />
-                  </ProtectedRoute>
-                }
+                element={<New inputData={userInputs} title="Add New User" />}
               />
             </Route>
             <Route path="products">
-              <Route
-                index
-                element={
-                  <ProtectedRoute>
-                    <List />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path=":productId"
-                element={
-                  <ProtectedRoute>
-                    <Single />
-                  </ProtectedRoute>
-                }
-              />
+              <Route index element={<List />} />
+              <Route path=":productId" element={<Single />} />
               <Route
                 path="new"
                 element={
-                  <ProtectedRoute>
-                    <New inputData={productInputs} title="Add New Product" />
-                  </ProtectedRoute>
+                  <New inputData={productInputs} title="Add New Product" />
                 }
               />
             </Route>
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,40 +1,6 @@
-import axios from "axios";
-import { useContext, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { AuthContext } from "../../context/AuthContext";
 import "./login.scss";
 
 const Login = () => {
-  const [credentials, setCredentials] = useState({
-    username: undefined,
-    password: undefined,
-  });
-  const { loading, error, dispatch } = useContext(AuthContext);
-  const navigate = useNavigate();
-  const handleChange = (e) => {
-    setCredentials((prev) => ({ ...prev, [e.target.id]: e.target.value }));
-  };
-  const handleClick = async (e) => {
-    e.preventDefault();
-    dispatch({
-      type: "LOGIN_START",
-    });
-    try {
-      const res = await axios.post("http://localhost:8800/api/auth/login", credentials);
-      console.log(res.data)
-      if (res.data.isAdmin) {
-        dispatch({ type: "LOGIN_SUCCESS", payload: res.data.details });
-        navigate("/");
-      } else {
-        dispatch({
-          type: "LOGIN_FAILURE",
-          payload: { message: "You are not allowed" },
-        });
-      }
-    } catch (err) {
-      dispatch({ type: "LOGIN_FAILURE", payload: err.response.data });
-    }
-  };
   return (
     <div className="login">
       <div className="lContainer">
@@ -43,19 +9,14 @@ const Login = () => {
           placeholder="username"
           type="text"
           id="username"
-          onChange={handleChange}
         />
         <input
           className="lInput"
           placeholder="password"
           type="password"
           id="password"
-          onChange={handleChange}
         />
-        <button disabled={loading} onClick={handleClick} className="lButton">
-          Login
-        </button>
-        {error && <span>{error.message}</span>}
+        <button className="lButton">Login</button>
       </div>
     </div>
   );
